7ad07823739d11978f3c267d2312c2b5a55980a9
AlejandroSava <alexandu.choric@gmail.com>
2025-10-07 07:35:18 -0600

    adding freeRTOS tasks and how to handle the cores

--- 11_freertos_tasks/main/11_freertos_tasks.c
+++ 11_freertos_tasks/main/11_freertos_tasks.c
@@ -24,6 +24,14 @@ void task2(void* params){ //create the task2
     }
 }
 
+void task3(void* params){ //create the task3
+    while(true){
+        printf("second core: %s\n", (char*) params);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+    }
+}
+
 void app_main(void)
 {   esp_log_level_set("*", ESP_LOG_ERROR);
     ESP_LOGI(TAG, "--- FreeRTOS Example ---");
@@ -31,4 +39,8 @@ void app_main(void)
     // run tasks at the same time
     xTaskCreate(&task1, "temperature reading", 2048, "task1", 2, NULL);
     xTaskCreate(&task2, "humidity reading", 2048, "task2", 2, NULL);
+
+    // by default the esp32 only use the first core that is for applicactions.
+    // to use the second core, we can use the next one 
+    xTaskCreatePinnedToCore(&task3, "core 2", 2048, "task3", 2, NULL,1); // the latest number points to core 2, 1 --> core 2, 0 -- > core 1
 }
